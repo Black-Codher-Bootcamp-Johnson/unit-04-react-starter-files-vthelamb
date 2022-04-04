38bdbf0f8ae2d19dda6ca336aeef65d1f9c5c72e
vthelamb <vglamb8@gmail.com>
2022-04-04 20:23:18 -0300

    last styling edits

--- session05/bookcaseapp/src/components/Book.js
+++ session05/bookcaseapp/src/components/Book.js
@@ -2,17 +2,16 @@ import React from "react";
 
 const Book = (props) => {
   return (
-    <div>
+    <div className="book">
       <img src={props.book.volumeInfo.imageLinks.thumbnail} />
       <h2>{props.book.volumeInfo.title}</h2>
       <h3>{props.book.volumeInfo.authors}</h3>
       <p>
-        {props.book.saleInfo.retailPrice
+        £{props.book.saleInfo.retailPrice
           ? props.book.saleInfo.retailPrice.amount
           : "No price defined"}
       </p>
-      <p>{props.book.volumeInfo.subtitle}</p>
-      <p>{props.book.volumeInfo.description}</p>
+      <p className='description'>{props.book.volumeInfo.description}</p>
         {props.removeBook ? (
         <button onClick={() => props.removeBook(props.book.id)} className="remove-button">Remove</button>
         ) : (
